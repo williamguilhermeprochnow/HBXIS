6e879a55c21e48acdbcd24fc1294444b163c8ea4
William Guilherme Prochnow <w.g.p6calmo@gmail.com>
2020-08-10 08:29:49 -0300

    Aplicação desenvolvida na NLW - Rocketseat

---
+++ nlw/public/scripts/addField.js
@@ -0,0 +1,30 @@
+// Procurar o botão
+
+document.querySelector("#add-time")
+
+// Quando clicar no botão
+
+.addEventListener('click', cloneField)
+
+// Executar uma ação
+
+function cloneField() {
+
+    // Duplicar os campos. Quais campos?
+
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true) //bool
+    
+    //Pegar os campos. Quais campos?
+
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    // Para cada campo, limpar
+
+    fields.forEach(function(field) { //Pegar o field do momento e limpa ele
+        field.value = ""
+    })
+
+    // Colocar na página. Onde?
+
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
---
+++ nlw/src/server.js
@@ -0,0 +1,36 @@
+// Servidor
+
+const express = require('express')
+const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
+
+// Configurar nunjucks (templates engine)
+
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+// Inicio e configuração do servidor
+
+server
+// Receber os dados do req.body
+
+.use(express.urlencoded({ extended: true }))
+
+// Configurar arquivos estáticos (css, scripts, imagens)
+
+.use(express.static("public"))
+
+// Rotas da aplicação
+
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+
+// Start do servidor
+
+.listen(5500)
